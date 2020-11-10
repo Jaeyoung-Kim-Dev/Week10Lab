c53848bce2a26d07e1cd7ca795893a7a6471dca6
Jaeyoung Kim <50346571+Jaeyoung-Kim-Dev@users.noreply.github.com>
2020-11-10 10:15:02 -0700

    Revert "Repace the hard code email with userDB in AdminFilter"
    This reverts commit ae32a325c212ad56bef8ae2c802c718b0baa7d24.

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,6 +1,5 @@
 package filters;
 
-import dataaccess.UserDB;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -11,7 +10,6 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import models.User;
 
 public class AdminFilter implements Filter {
 
@@ -24,11 +22,8 @@ public class AdminFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpSession session = httpRequest.getSession();
         String email = (String) session.getAttribute("email");
-        UserDB userDB = new UserDB();
-        User user = userDB.get(email);
-        int role = user.getRole().getRoleId();
 
-        if (role != 1) {
+        if (!"cprg352+admin@gmail.com".equals(email)) {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             httpResponse.sendRedirect("notes");
             return;

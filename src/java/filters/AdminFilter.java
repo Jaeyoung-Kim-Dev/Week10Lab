ae32a325c212ad56bef8ae2c802c718b0baa7d24
Jaeyoung Kim <50346571+Jaeyoung-Kim-Dev@users.noreply.github.com>
2020-11-10 03:41:04 -0700

    Repace the hard code email with userDB in AdminFilter

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -1,5 +1,6 @@
 package filters;
 
+import dataaccess.UserDB;
 import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,6 +11,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import models.User;
 
 public class AdminFilter implements Filter {
 
@@ -22,8 +24,11 @@ public class AdminFilter implements Filter {
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpSession session = httpRequest.getSession();
         String email = (String) session.getAttribute("email");
+        UserDB userDB = new UserDB();
+        User user = userDB.get(email);
+        int role = user.getRole().getRoleId();
 
-        if (!"cprg352+admin@gmail.com".equals(email)) {
+        if (role != 1) {
             HttpServletResponse httpResponse = (HttpServletResponse) response;
             httpResponse.sendRedirect("notes");
             return;
